37ca3aa35b9992c5adc13c2ac4f9725e01aefa2a
charlie <gn4rtistic@gmail.com>
2022-09-13 01:23:53 -0500

    created and styled contact me page

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -3,6 +3,7 @@ import './index.scss';
 import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBootstrap, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 
 const About = () =>
@@ -18,6 +19,7 @@ const About = () =>
     }, [] );
 
     return (
+        <>
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
@@ -55,6 +57,8 @@ const About = () =>
                 </div>
                 </div>
         </div>
+            <Loader type="line-scale-pulse-out-rapid" />
+        </>
     )
 }
 
--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 const Home = () =>
 {
@@ -21,6 +22,7 @@ const Home = () =>
     }, []);
 
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -34,7 +36,9 @@ const Home = () =>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
             <Logo/>
-        </div>
+            </div>
+            <Loader type="line-scale-pulse-out-rapid" />
+            </>
     )
 
 }
